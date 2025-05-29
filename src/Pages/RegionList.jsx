468587f870d84fe20db580ea1a379402b0816d2e
Eliana Depietri <eliana.depietri@gmail.com>
2025-05-29 14:52:54 +0200

    added addRegion button in RegionList, updated addRegion form with styling, toast and redirect not yet

--- src/Pages/RegionList.jsx
+++ src/Pages/RegionList.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 import api from "../services/api";
 import RegionCard from "../components/RegionCard";
@@ -35,6 +36,9 @@ export default function RegionList() {
   return (
     <div className="max-w-3xl mx-auto p-6 space-y-4">
       <h1 className="text-3xl font-bold">All regions</h1>
+      <Link 
+        to="/add-region"
+        className="bg-[#764134] text-white px-6 py-2 rounded-lg hover:bg-[#5e332a] transition">Add New Region</Link>
       {regions.length === 0 ? (
         <p>No regions yet</p>
       ) : (
